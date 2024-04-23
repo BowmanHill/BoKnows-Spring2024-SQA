79d168b0c50690a9f6a4f5b44be70bd6c101ded4
Henry Bowman Hill <HenryBowmanHill@outlook.com>
2024-04-23 10:33:11 -0500

    imple change 2

---
+++ BoKnows-Spring2024-SQA/fuzz.py
@@ -0,0 +1,32 @@
+from empirical.report import Average, Median
+from mining.mining import makeChunks
+import random
+
+# Average and Median from the report.py take a list as a parameter
+
+def simpleFuzzer():
+    ls = [145.3,'123', 123]
+
+    # Initialize a list to store crash messages
+    crash_messages = []
+
+    try:
+        # Call the fuzzValues function
+        Average(ls)
+    except Exception as e:
+            # Record the crash message
+        crash_messages.append(str(e))
+
+    try:
+        # Call the fuzzValues function
+        Median(ls)
+    except Exception as e:
+            # Record the crash message
+        crash_messages.append(str(e))
+    # Print the crash messages
+    print("Crash messages:")
+    for msg in crash_messages:
+        print(msg)
+
+if __name__ == '__main__':
+    simpleFuzzer()
