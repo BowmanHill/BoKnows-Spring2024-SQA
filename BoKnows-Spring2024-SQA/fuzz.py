e5c3cb23e25c76b39b2cea53ab0d10a840e24749
Henry Bowman Hill <HenryBowmanHill@outlook.com>
2024-04-30 08:27:46 -0500

    fuzzFinished

--- BoKnows-Spring2024-SQA/fuzz.py
+++ BoKnows-Spring2024-SQA/fuzz.py
@@ -1,26 +1,45 @@
-from empirical.report import Average, Median
+from empirical.report import Average, Median, reportProp
+from empirical.frequency import reportProportion
+import random
 
-# Average and Median from the report.py take a list as a parameter
 
 def simpleFuzzer():
     ls = [145.3,'123', 123]
 
     # Initialize a list to store crash messages
     crash_messages = []
-
+    
     try:
-        # Call the fuzzValues function
         Average(ls)
     except Exception as e:
             # Record the crash message
         crash_messages.append(str(e))
 
     try:
-        # Call the fuzzValues function
+        reportProportion(ls)
+    except Exception as e:
+            # Record the crash message
+        crash_messages.append(str(e))
+
+    try:
         Median(ls)
     except Exception as e:
             # Record the crash message
         crash_messages.append(str(e))
+        
+    try:
+        reportProp(ls)
+    except Exception as e:
+            # Record the crash message
+        crash_messages.append(str(e))
+        
+    try:
+        modls = ls + str( random.randint(1,10))
+        reportProportion(ls, modls)
+    except Exception as e:
+            # Record the crash message
+        crash_messages.append(str(e))        
+        
     # Print the crash messages
     print("Crash messages:")
     for msg in crash_messages:
